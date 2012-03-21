4f8414962c1fcbd9c611de7a028d5a998a952996
raghuram.sv@gmail.com <raghuram.sv@gmail.com@48b5fa17-06c9-fdef-3bb1-361cac12d200>
2012-03-21 05:59:39 +0000

    - Added ClientPacketParser.java

---
+++ client/src/cn/tinycontrol/client/packethandler/ClientPacketParser.java
@@ -0,0 +1,15 @@
+package cn.tinycontrol.client.packethandler;
+
+import cn.tinycontrol.common.model.FeedbackPacket;
+
+public class ClientPacketParser {
+    
+    public static FeedbackPacket constructPacket(byte[] feedbackBytes) {
+        return null;
+    }
+
+    public static byte[] constructBytes(FeedbackPacket feedbackPacket) {
+        return null;
+    }
+
+}
