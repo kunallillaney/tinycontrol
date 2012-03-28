fd5dcd22454d6e1f01fa57c27d3b4369a8f78e71
raghuram.sv@gmail.com <raghuram.sv@gmail.com@48b5fa17-06c9-fdef-3bb1-361cac12d200>
2012-03-28 03:14:28 +0000

    Added addData method

--- server/src/cn/tinycontrol/server/requesthandler/ServerWorkerThread.java
+++ server/src/cn/tinycontrol/server/requesthandler/ServerWorkerThread.java
@@ -1,6 +1,11 @@
 package cn.tinycontrol.server.requesthandler;
 
+import java.io.ByteArrayOutputStream;
 import java.net.InetAddress;
+import java.util.LinkedList;
+import java.util.Queue;
+
+import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream;
 
 import cn.tinycontrol.common.model.FeedbackPacket;
 
@@ -12,24 +17,43 @@ public class ServerWorkerThread implements Runnable {
     
     private int clientPort;
     
+    private boolean isShutDown = false;
+    
+    private ByteArrayOutputStream dataStream;
+    private Queue<FeedbackPacket> feedBackPacketQueue;
+    private int R;
+    private float X;
+    
     public ServerWorkerThread(int initialRTT, InetAddress clientAddr, int clientPort) {
         this.initialRTT = initialRTT;
         this.clientAddr = clientAddr;
         this.clientPort = clientPort;
+        
+        feedBackPacketQueue = new LinkedList<FeedbackPacket>();
+        dataStream = new ByteArrayOutputStream();
     }
     
     @Override
     public void run() {
         // TODO Auto-generated method stub
-        
+        while(!isShutDown) {
+            // Keep Sending data packets at rate X
+            
+        }
+        // Close connections and so on
+        // End the thread.
+    }
+    
+    public void addData(byte[] data, int offset, int length) {
+        // Add the data to the buffer
     }
     
     public void handleFeedBackPacket(FeedbackPacket clientPacket) {
-        
+        // Add the feedback packet to the queue and notify the run method and notify run() about the new sending rate X.
     }
     
     public void shutDown() {
-        
+        isShutDown = true;
     }
-
+    
 }
