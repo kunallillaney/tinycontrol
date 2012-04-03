7ff8206730f991506cd64b235599589aaf8bb596
p.adityarao@gmail.com <p.adityarao@gmail.com@48b5fa17-06c9-fdef-3bb1-361cac12d200>
2012-04-03 12:52:37 +0000

    NoFeedbackTimer added + getMax in CurrentState

---
+++ trunk/server/src/cn/tinycontrol/server/requesthandler/NoFeedbackTimer.java
@@ -0,0 +1,28 @@
+package cn.tinycontrol.server.requesthandler;
+
+public class NoFeedbackTimer {
+
+	public float xRecv; // in bps
+	public int timerLimit; //
+
+	public void updateLimit(int tLimit) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void startTimer() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void stopTimer() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void resetTimer() {
+		// TODO Auto-generated method stub
+
+	}
+	
+}
